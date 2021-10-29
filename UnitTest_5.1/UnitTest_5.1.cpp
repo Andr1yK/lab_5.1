6eb2390f7bafbd676600dfbde5a25373da56a244
Andriy Kobetiak <andriykobetiak@gmail.com>
2021-10-29 23:04:24 +0300

    Добавьте GITIGNORE- и GITATTRIBUTES-файлы.

---
+++ UnitTest_5.1/UnitTest_5.1.cpp
@@ -0,0 +1,41 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+#include "../lab_5.1/lab_5.1.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest51
+{
+	TEST_CLASS(UnitTest51)
+	{
+	public:
+		
+		TEST_METHOD(Test111)
+		{
+			double t;
+			t = f(1, 1, 1);
+			Assert::AreEqual(t, 2 * sin(1) + 1);
+		}
+
+		TEST_METHOD(Test011)
+		{
+			double t;
+			t = f(0, 1, 1);
+			Assert::AreEqual(t, 1.0);
+		}
+
+		TEST_METHOD(Test0_5)
+		{
+			double t;
+			t = f(1, 0.5, 1);
+			Assert::AreEqual(t, sin(0.5)+0.5*sin(1)+1);
+		}
+
+		TEST_METHOD(Test000)
+		{
+			double t;
+			t = f(0, 0, 0);
+			Assert::AreEqual(t, 0.);
+		}
+	};
+}
---
+++ lab_5.1/lab_5.1.cpp
@@ -0,0 +1,31 @@
+﻿// lab_5.1.cpp
+// Кобетяк Андрій
+// Лабораторна робота № 5.1
+// Функції, що містять арифметичний вираз
+// Варіант 12
+
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+double f(const double a, const double b, const double c);
+
+int main()
+{
+	double s, t;
+	
+	cout << "s = "; cin >> s;
+	cout << "t = "; cin >> t;
+	
+	double result = (f(1, t + s, s) + f(t, s * t, 1)) / (1 + pow(f(s, 1, t), 2));
+	
+	cout << "result = " << result << endl;
+
+	return 0;
+}
+
+double f(const double a, const double b, const double c)
+{
+	return a * sin(b) + b * sin(a) + c*c;
+}
